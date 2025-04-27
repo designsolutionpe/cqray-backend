73cbf34137af0c1fe2147cc5de326c4ac2799d37
SaidSuyv <sirsuyvi@gmail.com>
2025-04-26 20:54:43 -0500

    fixes & modifications

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,9 @@ use App\Http\Controllers\UnidadMedidaArticuloController;
 // Route::middleware('auth:sanctum')->get('/sedes', [SedeController::class, 'index']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::get('/images/{path}',function($path){
-  $path = storage_path("app/public/$path");
+Route::get('/images/{path}/{image}',function($path,$image){
+  \Log::info('check image path',['data'=>$path]);
+  $path = public_path("storage/$path/$image");
   if(file_exists($path))
     return response()->file($path);
   return response()->json(['error'=>'Image not found'], 404);
